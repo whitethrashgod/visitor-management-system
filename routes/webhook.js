6507f241f5411906ec92737e1fa20cceb8babd2d
whitethrashgod <hemasri25.k@gmail.com>
2026-06-08 16:59:32 +0530

    second commit

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,10 +1,27 @@
 const express = require("express");
-
 const router = express.Router();
 
+const VERIFY_TOKEN = "vms_token";
+
+router.get("/", (req, res) => {
+    const mode = req.query["hub.mode"];
+    const token = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
+
+    if (
+        mode === "subscribe" &&
+        token === VERIFY_TOKEN
+    ) {
+        return res.status(200).send(challenge);
+    }
+
+    return res.sendStatus(403);
+});
+
 router.post("/", (req, res) => {
-    console.log("Webhook Received");
-    console.log(req.body);
+    console.log(
+        JSON.stringify(req.body, null, 2)
+    );
 
     res.sendStatus(200);
 });
